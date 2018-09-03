9dc35054bafa3ad19d019d96a0256691ee1ed132
josiahnormand <josiah_n@yahoo.com>
2018-09-03 11:10:45 -0500

    test redux

---
+++ ReduxSimpleApp/src/containers/book-list.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+
+class BookList extends Component {
+//    constructor(props){
+//        super(props);
+//
+//        this.state = { term: '' };
+//    }
+
+    render() {
+        return (
+            <ul className="list-group col-sm-4" >
+                {this.renderList()}
+            </ul>
+ 
+        
+        );
+
+  
+    }
+
+    renderList() {
+        return this.props.books.map((book) => {
+            return (
+                <li key={book.title} className="list-group-item">{book.title}</li>
+            );
+        });
+    };
+
+}
+
+function mapStateToProps(state) {
+    //Return will show up as props 
+    //this is the glue between react and redux
+    console.log(state);
+    return{
+        books: state.books
+    
+    };
+
+
+}
+
+export default connect(mapStateToProps)(BookList);
